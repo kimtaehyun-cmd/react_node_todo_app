0a54f88a78bd12a70bec8e267383b4b2b9f12570
kimtaehyun-cmd <xogus_04@naver.com>
2024-08-12 15:31:28 +0900

    initial commit

---
+++ front/src/components/ItemPanel.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+const ItemPanel = () => {
+  const userInfo = useSelector((state) => state.auth.userToken);
+
+  console.log(userInfo);
+  return (
+    <div className="panel">
+      {userInfo ? (
+        <h2>{userInfo.userName}님 로그인 되었습니다.</h2>
+      ) : (
+        <h2>로그인이 필요합니다.</h2>
+      )}
+    </div>
+  );
+};
+
+export default ItemPanel;
