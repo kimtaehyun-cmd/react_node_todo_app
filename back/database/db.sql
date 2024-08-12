78c464e7ae63b5f08e443a41a8600fa2847295c0
kimtaehyun-cmd <xogus_04@naver.com>
2024-08-12 17:36:12 +0900

    first file

---
+++ back/database/db.sql
@@ -0,0 +1,22 @@
+-- 테이블 생성
+CREATE TABLE task (
+    _id TEXT PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    date TEXT NOT NULL,
+    isCompleted BOOLEAN NOT NULL DEFAULT false,
+    isImportant BOOLEAN NOT NULL DEFAULT false,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    userId TEXT NOT NULL
+);
+
+-- 데이터 추가
+INSERT INTO task (_id, title, description, date, isCompleted, isImportant, userId)
+VALUES('12345', '할일2', '할일2 설명', '2024-08-12', false, true, 'taehyun');
+
+-- 데이터 조회
+SELECT * FROM task
+
+-- 특정 사용자 데이터 필터 조회
+SELECT * FROM task WHERE userId = 'taehyuntech'
